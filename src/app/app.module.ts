9322c4bfe9f3acccb4e6b12f727250edefa91a10
Aleksa Janic <aleksajanic94@gmail.com>
2020-07-26 21:11:27 +0200

    Added  registration form and configured angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,12 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ShellComponent } from './shell/shell.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -26,7 +31,12 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserRegistration } from './user-registration';
+import { FormControl, Validators, FormGroup, AbstractControl } from '@angular/forms';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  userRegistration: UserRegistration;
+  registrationForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ])
+  });
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.userRegistration = new UserRegistration('', '', '');                                             
   }
 
+  checkEmailValidation(attribute: string): boolean {
+    return this.registrationForm.get(attribute).hasError('email') && !this.checkRequiredValidation(attribute);
+  }
+
+  checkRequiredValidation(attribute: string): boolean {
+    return this.registrationForm.get(attribute).hasError('required');
+  }
+
+  registerUser() {
+    if(!this.checkEmailValidation('email') && !this.checkRequiredValidation('username') && !this.checkRequiredValidation('password')) {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json'
+        })
+      };
+      this.http.post('http://localhost:9009/api/users/registration', this.userRegistration, httpOptions)
+      .subscribe(
+        error => console.log("Error: " + error)
+      )
+    }
+  }
 }
